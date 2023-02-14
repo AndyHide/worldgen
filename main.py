041dbc835ccfbe520c496f343cea1e421672ba1b
AndyHide <andyhidesds@gmail.com>
2023-02-14 11:38:45 +0500

    added borders calculation

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from math import sqrt
 from random import randint
 
 
-WIDTH = 10
+WIDTH = 30
 HEIGHT = 10
-NUM_PLATES = 10
+NUM_PLATES = 8
 
 
 class World:
@@ -19,6 +19,7 @@ class World:
     def show_plates(self):
         for plate in self.plates:
             print(f"x: {plate.x}, y: {plate.y}, weight: {plate.weight}, id: {plate.id}")
+        print("==========")
 
     def create_tiles(self):
         for y in range(HEIGHT):
@@ -36,11 +37,33 @@ class World:
             for tile in row:
                 print(tile, end='')
             print('')
+        print("==========")
 
     def find_borders(self):
         for row in self.tiles:
             for tile in row:
                 tile.set_borders(self.tiles)
+
+    def show_borders(self):
+        for row in self.tiles:
+            for tile in row:
+                print(f"{tile.border}", end='')
+            print('')
+        print("==========")
+
+    def show_x(self):
+        for row in self.tiles:
+            for tile in row:
+                print(f"{tile.x}", end='')
+            print('')
+        print("==========")
+
+    def show_y(self):
+        for row in self.tiles:
+            for tile in row:
+                print(f"{tile.y}", end='')
+            print('')
+        print("==========")
 
 
 class Plate:
@@ -66,34 +89,42 @@ class Tile:
         host_plate = 0
         for plate in plates:
             distance = sqrt((self.x - plate.x) * (self.x - plate.x) + (self.y - plate.y) * (self.y - plate.y))
-            wdistance = distance * plate.weight
-            if wdistance < min_distance or host_plate == 0:
+            weighted_distance = distance * plate.weight
+            if weighted_distance < min_distance or host_plate == 0:
                 host_plate = plate.id
-                min_distance = wdistance
+                min_distance = weighted_distance
         self.plate = host_plate
 
     def __str__(self):
         return str(self.plate)
 
     def set_borders(self, tiles):
-        if self.x > 0 and self.y > 0 and tiles[self.x-1][self.y-1].plate != self.plate:
-            self.border = 1
-        if self.x > 0 and tiles[self.x-1][self.y].plate != self.plate:
-            self.border = 1
-        if self.x > 0 and self.y < HEIGHT-1 and tiles[self.x-1][self.y+1].plate != self.plate:
-            self.border = 1
+        if self.x > 0:
+            if self.y > 0:
+                if self.plate != tiles[self.y-1][self.x-1].plate:
+                    self.border = 1
+            if self.plate != tiles[self.y][self.x-1].plate:
+                self.border = 1
+            if self.y < HEIGHT-1:
+                if self.plate != tiles[self.y+1][self.x-1].plate:
+                    self.border = 1
 
-        if self.y > 0 and tiles[self.x][self.y-1].plate != self.plate:
-            self.border = 1
-        if self.y < HEIGHT-1 and tiles[self.x][self.y+1].plate != self.plate:
-            self.border = 1
+        if self.y > 0:
+            if self.plate != tiles[self.y-1][self.x].plate:
+                self.border = 1
+        if self.y < HEIGHT-1:
+            if self.plate != tiles[self.y+1][self.x].plate:
+                self.border = 1
 
-        if self.x < WIDTH-1 and self.y > 0 and tiles[self.x+1][self.y-1].plate != self.plate:
-            self.border = 1
-        if self.x < WIDTH-1 and tiles[self.x+1][self.y].plate != self.plate:
-            self.border = 1
-        if self.x < WIDTH-1 and self.y < HEIGHT-1 and tiles[self.x+1][self.y+1].plate != self.plate:
-            self.border = 1
+        if self.x < WIDTH-1:
+            if self.y > 0:
+                if self.plate != tiles[self.y-1][self.x+1].plate:
+                    self.border = 1
+            if self.plate != tiles[self.y][self.x+1].plate:
+                self.border = 1
+            if self.y < HEIGHT-1:
+                if self.plate != tiles[self.y+1][self.x+1].plate:
+                    self.border = 1
 
 
 if __name__ == '__main__':
@@ -102,4 +133,8 @@ if __name__ == '__main__':
     world.show_plates()
     world.create_tiles()
     world.show_tiles()
+    # world.show_borders()
     world.find_borders()
+    world.show_borders()
+    # world.show_x()
+    # world.show_y()
